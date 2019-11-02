c34a57bdb0870f2e8c62d6b5e88f2dc40f68b2da
pengrui3 <pengrui3@xiaomi.com>
2019-11-02 18:28:03 +0800

    feature: add resize && canvas

--- index.js
+++ index.js
@@ -1,24 +1,85 @@
 import './style.scss'
+// https://github.com/parcel-bundler/parcel/issues/1668
+import images from './images/*.jpg'
+console.log('HelloWorld!')
 
-function render () {}
+var img = document.getElementById('view')
+var index = 1
+var add = 1
 
-  var img = document.getElementById('view');
-  var index = 1;
-  var add  = 1;
-
-  // setInterval(function(){
-  index += add;
+setInterval(() => {
+  index += add
   if (index > 131) {
-    add = add * -1;
+    add = add * -1
   }
   if (index < 0) {
-    add = add * -1;
-    index = 1;
+    add = add * -1
+    index = 1
   }
-  var  j = ("" + index).padStart(4, '0');
-  img.src = `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/02-head-bob-turn/${j}.jpg`;
-  console.log(j);
-  // }, 30);
-window.onload = function () {
+  var j = ('' + index).padStart(4, '0')
+  // img.src = images[j]
+  // console.log(j)
+  draw(images[j])
+}, 90)
 
+function draw(imgSrc) {
+  var context = canvas2.getContext('2d')
+  var img = new Image()
+
+  img.onload = function() {
+    canvas2.width = img.width
+    canvas2.height = img.height
+
+    context.drawImage(img, 0, 0)
+  }
+
+  img.src = imgSrc
 }
+
+var canvas2 = document.getElementById('02-head-bob-turn')
+
+var appleAirPodsPro = {
+  init() {
+    this.initData()
+    this.handleResize()
+    this.bindEvents()
+  },
+
+  initData() {
+    this.canvas2 = document.getElementById('02-head-bob-turn')
+  },
+
+  bindEvents() {
+    window.onresize = this.handleResize
+    window.scroll = this.handleScroll
+  },
+
+  handleScroll() {
+    console.log(111);
+    
+  },
+
+  handleResize() {
+    const wScale = window.innerWidth / (canvas2.width || 1458)
+    const hScale = (window.innerHeight - 52) / (canvas2.height || 1458)
+
+    canvas2.style.transform = `matrix(${wScale}, 0, 0, ${hScale}, 0, 0)`
+    // canvas2.style.cssText = `transform:matrix(1.18171, 0, 0, 1.18171, 0, 0);opacity: 1;`
+  },
+
+  drawCanvas(imgSrc) {
+    var context = canvas2.getContext('2d')
+    var img = new Image()
+
+    img.onload = function() {
+      canvas2.width = img.width
+      canvas2.height = img.height
+
+      context.drawImage(img, 0, 0)
+    }
+
+    img.src = imgSrc
+  },
+}
+
+appleAirPodsPro.init()
